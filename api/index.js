0aaac46d30fbfe54e80040790e4068b1133bf95f
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2023-01-17 11:03:15 -0300

    estruturando o projeto

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyparser from "body-parser";
+const port = 3000
+const app = express()
+app.use(bodyparser.json())
+
+app.get('/teste', (req, res) => {
+    res.status(200).send("Teste concluido")
+})
+app.listen(port, () => console.log(`Servidor rodando em ${3000}`))
