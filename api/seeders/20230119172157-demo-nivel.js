cc156ed83a10155f3489defe25af8be603ac47b1
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2023-01-19 14:42:18 -0300

    Populando com os seeders

--- api/seeders/20230119172157-demo-nivel.js
+++ api/seeders/20230119172157-demo-nivel.js
@@ -3,23 +3,26 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Niveis', [
+			{
+				descr_nivel: 'básico',
+				createdAt: new Date(),
+				updatedAt: new Date()			
+			},
+			{
+				descr_nivel: 'intermediário',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				descr_nivel: 'avançado',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			} 
+	], {})
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Niveis', null, {})
   }
 };
--- api/seeders/20230119172255-demo-turmas.js
+++ api/seeders/20230119172255-demo-turmas.js
@@ -3,23 +3,39 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Turmas', [
+			{
+				data_inicio: "2020-02-01",
+				nivel: 1,
+				docente: 6,
+				createdAt: new Date(),
+				updatedAt: new Date()				 
+			},
+			{
+				data_inicio: "2020-02-01",
+				nivel: 2,
+				docente: 5,
+				createdAt: new Date(),
+				updatedAt: new Date()			
+			},
+			{
+				data_inicio: "2020-02-01",
+				nivel: 3,
+				docente: 6,
+				createdAt: new Date(),
+				updatedAt: new Date()			
+				},
+			{
+				data_inicio: "2020-07-01",
+				nivel: 3,
+				docente: 6,
+				createdAt: new Date(),
+				updatedAt: new Date()			
+			}
+		], {})
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Turmas', null, {})
   }
 };
