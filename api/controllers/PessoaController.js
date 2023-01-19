647439c3970604faf8261466c408c005631ca4c7
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2023-01-19 10:19:21 -0300

    Criando o metodo put e delete e configurando suas rotas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -27,5 +27,25 @@ class PessoaController {
             return res.status(500).json(error.message)
         }
     }
+    static async atualizaPessoa(req, res){
+        const { id } = req.params
+        const pessoa = req.body
+        try{
+            await database.Pessoas.update(pessoa, {where: {id:Number(id)}})//Update retorna 0 ou 1, então nao faz sentido guardar o valor em uma variavel
+            const pessoaAtualizada = await database.Pessoas.findOne({where: {id:Number(id)}})
+            return res.status(200).json(pessoaAtualizada)
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
+    static async apagaPessoa(req, res){
+        const { id } = req.params
+        try{
+            await database.Pessoas.destroy({where: {id:Number(id)}})
+            return res.status(200).send(`Pessoa ${id} foi apagada`)
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
 }
 module.exports = PessoaController
--- api/routes/PessoaRoute.js
+++ api/routes/PessoaRoute.js
@@ -6,4 +6,6 @@ router
     .get("/pessoas", PessoaController.pegaTodasAsPessoas)
     .get("/pessoas/:id", PessoaController.pegaUmaPessoa)
     .post("/pessoas", PessoaController.criaPessoa)
+    .put("/pessoas/:id", PessoaController.atualizaPessoa)
+    .delete("/pessoas/:id", PessoaController.apagaPessoa)
 module.exports = router
