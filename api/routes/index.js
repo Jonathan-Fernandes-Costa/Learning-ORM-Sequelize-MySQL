8027a2aae6e346b04141faecefc5d2d1a4244c00
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2023-01-20 14:58:16 -0300

    Criando o router de Turmas

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,16 @@
 const bodyparser = require('body-parser')
 const pessoas = require('./PessoaRoute.js')
+const niveis = require('./NivelRoute.js')
+const turmas = require('./TurmaRoute.js')
 
 
 module.exports = app => {
-    app.use(bodyparser.json(), pessoas)
+    app.use(
+    bodyparser.json(), 
+    pessoas,
+    niveis,
+    turmas
+    )
 
 
 }
